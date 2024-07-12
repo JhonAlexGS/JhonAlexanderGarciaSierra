5a64c2123e7f139b84bed59d97e819dbe6401011
Jhon Alexander Garcia Sierra <jhon@booklick.co>
2024-07-12 08:51:41 -0500

    Template to Index

---
+++ src/Intro/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+  // import './App.css';
+// import "./css/prueba.css"
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default App;
